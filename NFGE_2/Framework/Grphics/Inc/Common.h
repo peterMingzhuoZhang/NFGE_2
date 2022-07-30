764ee135604ac53eb8a3377f6f3f6842cb5d758e
Peter Zhang <zhangpeter96@hotmail.com>
2022-07-30 00:00:42 -0400

    Adding graphic library

---
+++ NFGE_2/Framework/Grphics/Inc/Common.h
@@ -0,0 +1,23 @@
+//====================================================================================================
+// Filename:	Common.h
+// Created by:	Mingzhuo Zhang
+// Date:		2022/7
+//====================================================================================================
+#pragma once
+
+// Engine headers
+#include <Core/Inc/Core.h>
+#include <NFGEMath/Inc/NFGEMath.h>
+
+// DirectX headers
+#include <d3d12.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+
+// External headers
+#include <ImGui/Inc/imgui.h>
+
+// DirectX libraries
+#pragma comment(lib, "d3d12.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "dxguid.lib") // for DirectX reflect
